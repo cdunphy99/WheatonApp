a395cd95011371ddd03089a8f1feba5ca4a4441b
Chris <dunphy_christopher@wheatoncollege.edu>
2020-11-10 15:15:48 -0500

    Initial commit

---
+++ app/src/main/java/com/wheaton/wheatonapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.wheaton.wheatonapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    protected void pushData(String name, String text){
+        //Description:
+        //This function pushes data to the Firebase database ("Cloud Firestore") in the form of
+        //a Map containing the name of the note, the content or "text" of the note, and
+        //an auto-generated timestamp of the current time. Firestore automatically creates
+        //a unique ID for the note which can be used to recall or edit it directly.
+        //All Firebase data can be found inside our Firebase project named "cantaloupe"
+        //in our Google Firebase console page.
+        //Usage:
+        //pushdata("Grocery List", "eggs, ham, orange juice");
+        //May be changed later to store a json encoded string of data to support markdown.
+
+        Map<String, Object> note = new HashMap<>();
+
+        Date date = new Date();
+        Timestamp timeStamp = new Timestamp(date);
+
+        note.put("name", name);
+        note.put("text", text);
+        note.put("time", timeStamp);
+
+        db.collection("notes")
+                .add(note)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("firestore", "Note written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("firestore", "Error adding note to firestore", e);
+                    }
+                });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
