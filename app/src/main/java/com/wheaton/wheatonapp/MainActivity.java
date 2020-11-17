b643d9f8ccc50b9b0f284662736581d5fcda7259
Edje123 <edje321@gmail.com>
2020-11-16 22:31:37 -0500

    pullData function and random Id generator created, Id functionality added
    pullData may now be used to get notes from Firebase
    
    A unique Id is now created for the app and stored in sharedPrefs

--- app/src/main/java/com/wheaton/wheatonapp/MainActivity.java
+++ app/src/main/java/com/wheaton/wheatonapp/MainActivity.java
@@ -3,22 +3,33 @@ package com.wheaton.wheatonapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    String myId = "1";
 
     protected void pushData(String name, String text){
         //Description:
@@ -40,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         note.put("name", name);
         note.put("text", text);
         note.put("time", timeStamp);
+        note.put("id", myId);
 
         db.collection("notes")
                 .add(note)
@@ -57,10 +69,76 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    protected boolean pullData(){
+        //Description:
+        //This function pulls all notes from Firebase that have the the Id found in sharedPrefs.
+        //SharedPrefs will contain this instance of the app's unique Id (and if it contains nothing
+        //it will be updated to contain a random number that is not already used as an id in
+        //Firebase. For the time being the function simply prints each note to Logcat for testing
+        //purposes, but it will be updated to send all notes to an array for display in the app's
+        //main activity. This function returns a boolean that will be true when any notes are found
+        //with this app's Id. The function is also used to test if an Id is already used in Firebase.
+        //Usage:
+        //pullData();
+        //or
+        //if(pullData()){}
+
+        final boolean[] found = {false};
+
+        db.collection("notes")
+                .whereEqualTo("id", myId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("firestore", document.getId() + " => " + document.getData());
+                            }
+                            found[0] = true;
+                        } else {
+                            Log.d("firestore", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        if(found[0]){
+            return true;
+        }
+        else return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = this.getPreferences(Context.MODE_PRIVATE);
+
+        String retId = prefs.getString("wheaton_myId", "none");
+        Log.d("idWorks", retId);
+
+        if(retId.equals("none")){
+            Log.d("idWorks", "equals");
+
+            Random rand = new Random();
+            String sender = String.valueOf(rand.nextInt(999999999));
+
+            while(!pullData()){
+                sender = String.valueOf(rand.nextInt(999999999));
+            }
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("wheaton_myId", sender);
+            editor.apply();
+
+            String inId = prefs.getString("wheaton_myId", "none");
+            Log.d("idWorks", inId);
+
+            myId = inId;
+        }
+
+        pullData();
     }
 
 }
