2cfe316929e88f6f06dd2f990b28bfeeaf5afbe7
Michael <mfoley41100@gmail.com>
2020-11-19 15:17:51 -0500

    Pop up time

--- app/src/main/java/com/wheaton/wheatonapp/MainActivity.java
+++ app/src/main/java/com/wheaton/wheatonapp/MainActivity.java
@@ -2,11 +2,17 @@ package com.wheaton.wheatonapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -16,6 +22,7 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +30,17 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private WebView webView;
+    private boolean cardView_Shown = true;
+
+    RecyclerView recyclerView;
+    ArrayList<String> source;
+    RecyclerView.LayoutManager RecyclerViewLayoutManager;
+    recylerAdapter adapter;
+    LinearLayoutManager HorizontalLayout;
+
+    View ChildView;
+    int RecyclerViewItemPosition;
 
     protected void pushData(String name, String text){
         //Description:
@@ -73,6 +91,10 @@ public class MainActivity extends AppCompatActivity {
         final FloatingActionButton miniFAB1 = findViewById(R.id.miniFAB1);
         final FloatingActionButton miniFAB2 = findViewById(R.id.miniFAB2);
 
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
+
         mainFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,5 +114,50 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        miniFAB1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hide_showCardView();
+            }
+        });
+
+        //https://www.geeksforgeeks.org/android-horizontal-recyclerview-with-examples/
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
+        RecyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(RecyclerViewLayoutManager);
+        AddItemsToRecyclerViewArrayList();
+        adapter = new recylerAdapter(source);
+        HorizontalLayout = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(HorizontalLayout);
+        recyclerView.setAdapter(adapter);
+
+
     }
+
+    public void AddItemsToRecyclerViewArrayList() {
+        // Adding items to ArrayList
+        source = new ArrayList<>();
+        source.add("Sticky1");
+        source.add("Sticky2");
+        source.add("Sticky3");
+    }
+
+
+    public void click(View view) {
+        TextView t = (TextView) findViewById (R.id.textview);
+        Toast.makeText(getApplicationContext(), "You have pressed a sticky.".concat(t.getText().toString()), Toast.LENGTH_SHORT).show();
+    }
+
+    public void hide_showCardView(){
+        RelativeLayout r = (RelativeLayout) findViewById(R.id.rView);
+        if(cardView_Shown){
+            r.setVisibility(View.GONE);
+            cardView_Shown = false;
+        } else {
+            r.setVisibility(View.VISIBLE);
+            cardView_Shown = true;
+        }
+    }
+
 }
