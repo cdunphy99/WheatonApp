9e254dd478f40cb4e43fe6804b5c2fcd750694af
Michael <mfoley41100@gmail.com>
2020-12-04 13:53:34 -0500

    plz help Ethan

--- app/src/main/java/com/wheaton/wheatonapp/MainActivity.java
+++ app/src/main/java/com/wheaton/wheatonapp/MainActivity.java
@@ -9,12 +9,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -300,6 +302,8 @@ public class MainActivity extends AppCompatActivity {
     public void hide_showCardView(){
         RecyclerView rview = (RecyclerView) findViewById(R.id.recyclerview);
         Button addStickyButton = (Button) findViewById(R.id.addStickyButton);
+
+        //LinearLayout LL = (LinearLayout) findViewById(R.id.rView);
         if(cardView_Shown){
             rview.setVisibility(View.GONE);
             addStickyButton.setVisibility(View.GONE);
@@ -316,6 +320,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void add(View view){
         source.add(new StickyNoteObject("New Sticky", "New Message"));
+        if (source.get(0).getTitle().equals("EMPTY")){
+            source.remove(1);
+        }
         adapter.notifyDataSetChanged();
     }
 
